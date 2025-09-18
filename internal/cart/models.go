cfc2ed30d13299684489c8ffc496defb929e76ad
Pavel Khromykh <drykilllogic232a@gmail.com>
2025-09-18 21:41:29 +0300

    add swagger comments cart/models

--- internal/cart/models.go
+++ internal/cart/models.go
@@ -2,6 +2,8 @@ package cart
 
 import "time"
 
+// CartItem represents an item in the shopping cart
+// swagger:model CartItem
 type CartItem struct {
 	ID        int64     `db:"id" json:"id"`
 	UserID    int64     `db:"user_id" json:"user_id"`
--- migrations/004_create_cart.sql
+++ migrations/004_create_cart.sql
@@ -1,12 +1,11 @@
 -- +goose Up
-
 CREATE TABLE cart_items (
 	id SERIAL PRIMARY KEY,
 	user_id BIGINT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
     product_id BIGINT NOT NULL REFERENCES products(id) ON DELETE CASCADE,
     quantity INT NOT NULL CHECK (quantity > 0),
-	created_at TIMESTAMP DEFAULT NOW(),
-	updated_at TIMESTAMP DEFAULT NOW()
+	created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+	updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
 
 -- +goose Down
